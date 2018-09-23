880b972c37a671fc13d083160e32a72ce72676c9
Michael Harvey <michael4d45@gmail.com>
2018-09-22 21:02:11 -0600

    takes in commands and spits out ssh

--- sshm.sh
+++ sshm.sh
@@ -1,3 +1,26 @@
-#! /bin/bash
+#! /bin/bash	
+SSHARGS=""
+while [ ! $# -eq 0 ]
+do
+	echo $# args
+	echo $1 arg1
+	echo sshargs:\"$SSHARGS\"
+	case "$1" in
+		--help | -h)
+#			helpmenu
+			exit
+			;;
+		--take-over-world)
+			echo Done >&2
+			exit
+			;;
+		*)
+			echo adding to ssh command >&2
+			SSHARGS="$SSHARGS $1"
+			;;
+	esac
+	shift
+done
 
-
+echo sshargs:\"$SSHARGS\"
+ssh $SSHARGS 
